2950fa988d6060a2f09904efe84dd127d43a62d8
Poekes <sunday26261@gmail.com>
2026-06-27 23:18:35 +0700

    refactor(IbuSeeder): update seeding logic to use user ID

--- src/database/IbuSeeder.cpp
+++ src/database/IbuSeeder.cpp
@@ -4,14 +4,14 @@
 void IbuSeeder::seed() {
     if (!UserModel::userExists("ibu_budi")) {
         UserModel::registerUser(2, "ibu_budi", "123");
-        UserModel::linkIbuToAnak("ibu_budi", "Budi");
+        UserModel::linkIbuToAnak(UserModel::getUserIdByUsername("ibu_budi"), "Budi");
     }
     if (!UserModel::userExists("ibu_siti")) {
         UserModel::registerUser(2, "ibu_siti", "123");
-        UserModel::linkIbuToAnak("ibu_siti", "Siti");
+        UserModel::linkIbuToAnak(UserModel::getUserIdByUsername("ibu_siti"), "Siti");
     }
     if (!UserModel::userExists("ibu_andi")) {
         UserModel::registerUser(2, "ibu_andi", "123");
-        UserModel::linkIbuToAnak("ibu_andi", "Andi");
+        UserModel::linkIbuToAnak(UserModel::getUserIdByUsername("ibu_andi"), "Andi");
     }
 }
