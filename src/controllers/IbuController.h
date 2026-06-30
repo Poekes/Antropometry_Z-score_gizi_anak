4ce9f7f11c9d1e5732fbafb7b1994c5498d24d17
Poekes <sunday26261@gmail.com>
2026-07-01 00:26:23 +0700

    feat: tambah dependensi ReferenceData dan menu detail diagnosis untuk ibu

--- src/controllers/IbuController.h
+++ src/controllers/IbuController.h
@@ -2,18 +2,21 @@
 #define IBU_CONTROLLER_H
 
 #include "../models/UserModel.h"
+#include "../zscore_data.h"
 
 class IbuController {
 public:
-    IbuController(UserSession& session);
+    IbuController(UserSession& session, ReferenceData& refData);
     void run();
 
 private:
     UserSession& currentSession;
+    ReferenceData& refData;
 
     void showMainMenu();
     void doPilihAnak();
     void doLihatRiwayat();
+    void doLihatDetailDiagnosis();
 };
 
 #endif
